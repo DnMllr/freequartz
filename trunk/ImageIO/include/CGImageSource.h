dcfbbde6231479fc33c0ba4ab15a636d551beb11
v.richomme <v.richomme@2d173a58-942e-4657-f77f-4b59ac81e5fd>
2010-04-14 12:54:41 +0000

    move header inside include folder

---
+++ trunk/ImageIO/include/CGImageSource.h
@@ -0,0 +1,74 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Smartmobili.
+** All rights reserved.
+** Contact: Smartmobili (contact@smartmobili.com)
+**	
+** This file is part of the CoreGraphics module of the freequartz Toolkit.
+**
+**
+** GNU Lesser General Public License Usage	
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements	
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+****************************************************************************/
+
+typedef struct CGImageSource *CGImageSourceRef;
+
+enum CGImageSourceStatus {
+   kCGImageStatusUnexpectedEOF = -5,
+   kCGImageStatusInvalidData = -4,
+   kCGImageStatusUnknownType = -3,
+   kCGImageStatusReadingHeader = -2,
+   kCGImageStatusIncomplete = -1,
+   kCGImageStatusComplete = 0
+};
+typedef enum CGImageSourceStatus CGImageSourceStatus;
+
+
+//CFStringRef kCGImageSourceTypeIdentifierHint;
+//CFStringRef kCGImageSourceShouldAllowFloat;
+//CFStringRef kCGImageSourceShouldCache;
+//CFStringRef kCGImageSourceCreateThumbnailFromImageIfAbsent;
+//CFStringRef kCGImageSourceCreateThumbnailFromImageAlways;
+//CFStringRef kCGImageSourceThumbnailMaxPixelSize;
+//CFStringRef kCGImageSourceCreateThumbnailWithTransform
+
+
+
+CFDictionaryRef CGImageSourceCopyProperties (
+   CGImageSourceRef isrc,
+   CFDictionaryRef options
+);
+
+
+CFDictionaryRef CGImageSourceCopyPropertiesAtIndex (
+   CGImageSourceRef isrc,
+   size_t index,
+   CFDictionaryRef options
+);
+
+CFArrayRef CGImageSourceCopyTypeIdentifiers (
+   void
+);
+
+CGImageRef CGImageSourceCreateImageAtIndex (
+   CGImageSourceRef isrc,
+   size_t index,
+   CFDictionaryRef options
+);
+
+CGImageSourceRef CGImageSourceCreateIncremental (
+   CFDictionaryRef options
+);
+
+CGImageRef CGImageSourceCreateThumbnailAtIndex (
+   CGImageSourceRef isrc,
+   size_t index,
+   CFDictionaryRef options
+);
+
