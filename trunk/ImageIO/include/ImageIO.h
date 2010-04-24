1c14314e46b8bbc77246592735da29090e461b99
v.richomme <v.richomme@2d173a58-942e-4657-f77f-4b59ac81e5fd>
2010-04-24 19:48:44 +0000

    add new include ImageIO.h

---
+++ trunk/ImageIO/include/ImageIO.h
@@ -0,0 +1,34 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Smartmobili.
+** All rights reserved.
+** Contact: Smartmobili (contact@smartmobili.com)
+**	
+** This file is part of the ImageIO module of the freequartz Toolkit.
+**
+**
+** GNU Lesser General Public License Usage	
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements	
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+****************************************************************************/
+
+
+/* REFERENCES : 
+  http://www.devworld.apple.com/mac/library/documentation/GraphicsImaging/
+  Reference/ImageIORefCollection/ImageIORefCollection.pdf 
+*/
+#ifndef IMAGEIO_H_
+#define IMAGEIO_H_
+
+#include <CoreGraphics/CoreGraphics.h>
+#include <ImageIO/CGImageProperties.h>
+#include <ImageIO/CGImageSource.h>
+#include <ImageIO/CGImageDestination.h>
+
+
+#endif	/* IMAGEIO_H_ */
