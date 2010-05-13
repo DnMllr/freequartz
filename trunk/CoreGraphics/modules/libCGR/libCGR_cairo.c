010ee2398336594e34b90095719724eda2d73d5d
v.richomme <v.richomme@2d173a58-942e-4657-f77f-4b59ac81e5fd>
2010-05-13 15:00:48 +0000

    add ne project for cairo backend

---
+++ trunk/CoreGraphics/modules/libCGR/libCGR_cairo.c
@@ -0,0 +1,19 @@
+/****************************************************************************
+**
+** Copyright (C) 2010 Smartmobili.
+** All rights reserved.
+** Contact: Smartmobili (contact@smartmobili.com)
+**	
+** This file is part of the CoreGraphics module of the freequartz Toolkit.
+**
+**
+** GNU Lesser General Public License Usage	
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements	
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+****************************************************************************/
+
